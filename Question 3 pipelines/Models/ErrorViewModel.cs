16f22271c0dbb3b9c3d0ae45ef1727359014e856
okuhle24 <20241846@ctucareer.co.za>
2024-10-29 08:55:41 +0000

    Deploying to staging

--- Question 3 pipelines/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Question_3_pipelines.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
